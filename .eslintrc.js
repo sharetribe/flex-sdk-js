f95276eae7bdb821790a2a827effc701465ed56f
Mikko Koski <mikkokos@gmail.com>
2017-01-12 09:55:01 +0200

    Instead of defining globals, define jest environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,7 @@ module.exports = {
   env: {
     es6: true,
     browser: false,
-    node: false
+    node: false,
+    jest: true
   },
-  globals: {
-    //
-    // Specify globals needed by Mocha/Chai
-    // Set the value to `false` to prevent the variables
-    // to be overriden.
-    // See: http://eslint.org/docs/user-guide/configuring#specifying-globals
-    //
-    describe: false,
-    it: false,
-    expect: false
-  }
 };
