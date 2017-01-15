bb91721c4008493c7ec2c28bea6b1567675984f5
Mikko Koski <mikkokos@gmail.com>
2017-01-15 13:00:05 +0100

    Move transit to own file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import transit from 'transit-js';
 import { methodPath, assignDeep } from './utils';
-import { UUID } from './types';
+import { reader, writer } from './serializer';
 
 const defaultOpts = {
   headers: {'Accept': 'application/transit'},
@@ -10,40 +9,18 @@ const defaultOpts = {
 
 const defaultEndpoints = [];
 
-const ident = x => x;
-
-var transitReader = transit.reader("json", {
-  handlers: {
-    "u": (rep) => new UUID(rep),
-  },
-  arrayBuilder: {
-    init: function(node) { return []; },
-    add: function(ret, val, node) {
-      ret.push(val);
-      return ret; },
-    finalize: ident,
-  },
-  mapBuilder: {
-    init: function(node) { return {}; },
-    add: function(ret, key, val, node) {
-      ret[key.name()] = val;
-      return ret },
-    finalize: ident,
-  }
-});
-
 class SharetribeSdk {
   constructor(opts, endpoints) {
     this.opts = Object.freeze({ ...defaultOpts, ...opts });
 
+    const r = reader();
+    const w = writer();
+
     const axiosInstance = axios.create({
       headers: this.opts.headers,
       baseURL: this.opts.baseUrl,
-      transformRequest: [(data) => data],
-      transformResponse: [(data) => {
-        return transitReader.read(data);
-        // return JSON.parse(data);
-      }],
+      transformRequest: [(data) => w.write(data)],
+      transformResponse: [(data) => r.read(data)],
     });
 
     [...defaultEndpoints, ...endpoints].forEach((ep) => {
---
+++ src/serializer.js
@@ -0,0 +1,28 @@
+import transit from 'transit-js';
+import { UUID } from './types';
+
+const ident = x => x;
+
+export const reader = () => transit.reader("json", {
+  handlers: {
+    u: (rep) => new UUID(rep),
+  },
+  arrayBuilder: {
+    init: () => [],
+    add: (ret, val) => {
+      ret.push(val);
+      return ret;
+    },
+    finalize: ident,
+  },
+  mapBuilder: {
+    init: () => ({}),
+    add: (ret, key, val) => {
+      ret[key.name()] = val;
+      return ret;
+    },
+    finalize: ident,
+  }
+});
+
+export const writer = () => transit.writer("json");
