d07480c35e9097e559fc3f5edbe7f36461d77cc2
Mikko Koski <mikkokos@gmail.com>
2017-01-15 22:21:09 +0100

    Add support for custom writers/readers

--- src/serializer.js
+++ src/serializer.js
@@ -1,47 +1,117 @@
 import transit from 'transit-js';
+import _ from 'lodash';
 import { UUID } from './types';
 
 const ident = x => x;
 
-export const reader = () => transit.reader('json', {
-  handlers: {
-    u: rep => new UUID(rep),
+/**
+   Composes two readers (default and custom) so that:
 
-    // Convert keywords to plain strings
-    //
-    // TODO This is problematic. When we convert keywords to strings,
-    // we lose the information that the string was originally a
-    // keyword. Thus, `read(write(data)) !== data`. Is this a bad thing,
-    // I don't know yet? Can we make the server to threat Strings as if
-    // they were keywords, that's an open question.
-    ':': rep => rep,
-  },
-  arrayBuilder: {
-    init: () => [],
-    add: (ret, val) => {
-      ret.push(val);
-      return ret;
+  ```
+  const defaultReader = {
+     type: UUID,
+     reader: (rep) => new UUID(rep),
+  };
+
+  const customReader = {
+     type: UUID,
+     reader: (rep) => new MyCustomUuid(rep.uuid), // rep instanceof UUID === true
+  }
+
+  Composition creates a new reader:
+
+  {
+     type: UUID,
+     reader: (rep) => new MyCustomUuid(new UUID(rep))
+  }
+  ```
+ */
+
+const composeReader = (defaultReader, customReader) => {
+  const defaultReaderFn = defaultReader.reader;
+  const customReaderFn = customReader ? customReader.reader : _.identity;
+
+  return rep => customReaderFn(defaultReaderFn(rep));
+};
+
+const composeWriter = (defaultWriter, customWriter) => {
+  const defaultWriterFn = defaultWriter.writer;
+  const customWriterFn = customWriter ? customWriter.writer : _.identity;
+
+  return rep => defaultWriterFn(customWriterFn(rep));
+};
+
+const typeMap = {
+  u: UUID,
+};
+
+const defaultReaders = [{
+  type: UUID,
+  reader: rep => new UUID(rep),
+}];
+
+const defaultWriters = [{
+  type: UUID,
+  writer: v => v.uuid,
+}];
+
+export const reader = (customReaders = []) => {
+  const handlers = _.fromPairs(_.map(typeMap, (typeClass, tag) => {
+    const defaultReader = _.find(defaultReaders, r => r.type === typeClass);
+    const customReader = _.find(customReaders, r => r.type === typeClass);
+
+    return [tag, composeReader(defaultReader, customReader)];
+  }));
+
+  return transit.reader('json', {
+    handlers: {
+      ...handlers,
+
+      // Convert keywords to plain strings
+      //
+      // TODO This is problematic. When we convert keywords to strings,
+      // we lose the information that the string was originally a
+      // keyword. Thus, `read(write(data)) !== data`. Is this a bad thing,
+      // I don't know yet? Can we make the server to threat Strings as if
+      // they were keywords, that's an open question.
+      ':': rep => rep,
     },
-    finalize: ident,
-  },
-  mapBuilder: {
-    init: () => ({}),
-    add: (ret, key, val) => {
-      /* eslint-disable no-param-reassign */
-      ret[key] = val;
-      return ret;
+    arrayBuilder: {
+      init: () => [],
+      add: (ret, val) => {
+        ret.push(val);
+        return ret;
+      },
+      finalize: ident,
     },
-    finalize: ident,
-  },
-});
+    mapBuilder: {
+      init: () => ({}),
+      add: (ret, key, val) => {
+        /* eslint-disable no-param-reassign */
+        ret[key] = val;
+        return ret;
+      },
+      finalize: ident,
+    },
+  });
+};
 
-const uuidHandler = transit.makeWriteHandler({
-  tag: () => 'u',
-  rep: v => v.uuid,
-});
+export const writer = (customWriters = []) => {
+  const handlers = _.flatten(_.map(typeMap, (typeClass, tag) => {
+    const defaultWriter = _.find(defaultWriters, w => w.type === typeClass);
+    const customWriter = _.find(customWriters, w => w.type === typeClass);
+    const composedWriter = composeWriter(defaultWriter, customWriter);
+    const customTypeClass = customWriter ? customWriter.customType : defaultWriter.type;
 
-export const writer = () => transit.writer('json', {
-  handlers: transit.map([
-    UUID, uuidHandler,
-  ]),
-});
+    const handler = transit.makeWriteHandler({
+      tag: () => tag,
+      rep: composedWriter,
+    });
+
+    return [customTypeClass || typeClass, handler];
+  }));
+
+  return transit.writer('json', {
+    handlers: transit.map(handlers),
+  });
+};
--- src/serializer.test.js
+++ src/serializer.test.js
@@ -28,5 +28,50 @@ describe('serializer', () => {
 
     expect(r.read(w.write(testData))).toEqual(testData);
   });
+
+  it('allows you to add your own reader handlers for predefined types', () => {
+    class MyCustomUuid {
+      constructor(str) {
+        this.myCustomerUuidRepresentation = str;
+      }
+    }
+
+    const r = reader([
+      {
+        type: UUID,
+        reader: v => new MyCustomUuid(v.uuid),
+      },
+    ]);
+
+    const w = writer();
+
+    const data = '69c3d77a-db3f-11e6-bf26-cec0c932ce01';
+
+    expect(r.read(w.write({ id: new UUID(data) })).id)
+      .toEqual(new MyCustomUuid(data));
+  });
+
+  it.only('allows you to add your own writers handlers for predefined types', () => {
+    class MyCustomUuid {
+      constructor(str) {
+        this.myCustomUuidRepresentation = str;
+      }
+    }
+
+    const r = reader();
+
+    const w = writer([
+      {
+        type: UUID,
+        customType: MyCustomUuid,
+        writer: v => new UUID(v.myCustomUuidRepresentation),
+      },
+    ]);
+
+    const data = '69c3d77a-db3f-11e6-bf26-cec0c932ce01';
+
+    expect(r.read(w.write({ id: new MyCustomUuid(data) })).id)
+      .toEqual(new UUID(data));
+  });
 });
 
