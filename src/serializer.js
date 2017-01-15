044104be97904587c5afaf06b99bff79eec8541f
Mikko Koski <mikkokos@gmail.com>
2017-01-15 17:38:31 +0100

    Add UUID tests and UUID writer

--- src/serializer.js
+++ src/serializer.js
@@ -8,6 +8,12 @@ export const reader = () => transit.reader('json', {
     u: rep => new UUID(rep),
 
     // Convert keywords to plain strings
+    //
+    // TODO This is problematic. When we convert keywords to strings,
+    // we lose the information that the string was originally a
+    // keyword. Thus, `read(write(data)) !== data`. Is this a bad thing,
+    // I don't know yet? Can we make the server to threat Strings as if
+    // they were keywords, that's an open question.
     ':': rep => rep,
   },
   arrayBuilder: {
@@ -29,4 +35,13 @@ export const reader = () => transit.reader('json', {
   },
 });
 
-export const writer = () => transit.writer('json');
+const uuidHandler = transit.makeWriteHandler({
+  tag: () => 'u',
+  rep: v => v.uuid,
+});
+
+export const writer = () => transit.writer('json', {
+  handlers: transit.map([
+    UUID, uuidHandler,
+  ]),
+});
--- src/serializer.test.js
+++ src/serializer.test.js
@@ -1,4 +1,5 @@
 import { reader, writer } from './serializer';
+import { UUID } from './types';
 
 describe('serializer', () => {
   it('reads and writes transit', () => {
@@ -9,6 +10,17 @@ describe('serializer', () => {
       d: {
         e: true,
       },
+    };
+
+    const r = reader();
+    const w = writer();
+
+    expect(r.read(w.write(testData))).toEqual(testData);
+  });
+
+  it('handles UUIDs', () => {
+    const testData = {
+      id: new UUID('69c3d77a-db3f-11e6-bf26-cec0c932ce01'),
     };
 
     const r = reader();
