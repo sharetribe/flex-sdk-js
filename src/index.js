820c377f9f279da263ff7ce62ff4fa918ec27038
Mikko Koski <mikkokos@gmail.com>
2017-01-17 15:23:33 +0100

    Add comments to SDK initialization to make it easier to grasp

--- src/index.js
+++ src/index.js
@@ -17,6 +17,39 @@ const defaultEndpoints = [
 //   return data;
 // };
 
+const createSdkMethod = (req, axiosInstance) =>
+  (params = {}) =>
+    axiosInstance.request({ ...req, params });
+
+/**
+ * Mutates 'obj' by adding endpoint methods to it.
+ *
+ * @param {Object} obj - Object that will be assigned with the endpoints.
+ * @param {Object[]} endpoints - endpoint definitions
+ * @param {Object} axiosInstance
+ *
+ */
+const assignEndpoints = (obj, endpoints, axiosInstance) => {
+  endpoints.forEach((ep) => {
+    const req = {
+      url: ep.path,
+    };
+
+    const sdkMethod = createSdkMethod(req, axiosInstance);
+
+    // e.g. '/marketplace/users/show/' -> ['marketplace', 'users', 'show']
+    const path = methodPath(ep.path);
+
+    // Assign `sdkMethod` to path.
+    //
+    // E.g. assign obj.marketplace.users.show = sdkMethod
+    assignDeep(obj, path, sdkMethod);
+  });
+
+  // Return the mutated obj
+  return obj;
+};
+
 class SharetribeSdk {
 
   constructor(opts, endpoints, adapter, handlers) {
@@ -42,7 +75,7 @@ class SharetribeSdk {
     const r = reader(readers);
     const w = writer(writers);
 
-    const instanceOpts = {
+    const httpOpts = {
       headers: this.opts.headers,
       baseURL: this.opts.baseUrl,
       transformRequest: [
@@ -56,16 +89,11 @@ class SharetribeSdk {
       adapter,
     };
 
-    const axiosInstance = axios.create(instanceOpts);
+    const axiosInstance = axios.create(httpOpts);
+    const allEndpoints = [...defaultEndpoints, ...endpoints];
 
-    [...defaultEndpoints, ...endpoints].forEach((ep) => {
-      const req = {
-        url: ep.path,
-      };
-
-      assignDeep(this, methodPath(ep.path),
-                 (params = {}) => axiosInstance.request({ ...req, params }));
-    });
+    // Assign all endpoint definitions to 'this'
+    assignEndpoints(this, allEndpoints, axiosInstance);
   }
 }
 
