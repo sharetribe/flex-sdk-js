ed670c206cd596a75595e56f74bf84458bd01a1c
Mikko Koski <mikkokos@gmail.com>
2017-02-09 15:00:22 +0200

    Add an example that demonstrates how to cookie store works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import SharetribeSdk from './sdk';
 import * as types from './types';
+import browserCookieStore from './browser_cookie_store';
 
 const createInstance = config =>
   new SharetribeSdk(config);
+
+// Export token stores
+const tokenStore = {
+  browserCookieStore,
+};
 
 /* eslint-disable import/prefer-default-export */
 export {
   createInstance,
   types,
+  tokenStore,
 };
