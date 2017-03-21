e0f8c570b2a8799318670604b7e4b21b71a02576
Mikko Koski <mikkokos@gmail.com>
2017-03-21 11:43:32 +0200

    Add a variable to hold the current token
    - This prevents the issue where server was asking multiple new access tokens if multiple calls were made in the same Express.js request handler.

--- src/express_cookie_store.js
+++ src/express_cookie_store.js
@@ -5,7 +5,13 @@ const createStore = ({ clientId, req, res }) => {
   const namespace = 'st';
   const key = generateKey(clientId, namespace);
 
-  const getToken = () => {
+  // A mutable variable containing the current token.
+  // When a `setToken` is called, the current token will be
+  // stored to this variable. `getToken` will read subsequent
+  // calls from this variable.
+  let currentToken;
+
+  const readCookie = () => {
     const cookie = req.cookies[key];
 
     if (cookie) {
@@ -13,13 +19,21 @@ const createStore = ({ clientId, req, res }) => {
     }
 
     return null;
+  }
+
+  const getToken = () => {
+    currentToken = currentToken || readCookie();
+
+    return currentToken;
   };
 
   const setToken = (tokenData) => {
+    currentToken = tokenData;
     res.cookie(key, JSON.stringify(tokenData), { maxAge: 1000 * 60 * 60 * 24 * expiration });
   };
 
   const removeToken = () => {
+    currentToken = null;
     res.clearCookie(key);
   };
 
