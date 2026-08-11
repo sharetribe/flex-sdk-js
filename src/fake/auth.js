e78aa76305ce35dce88944eb3967121f808d7068
Mikko Koski <mikko@sharetribe.com>
2026-08-11 09:51:25 +0300

    MultitenantSDK: Test expired token
    - Improve multitenantSDK fake: Expired tokens return 401 as they do with the
    real API
    - Add a (temporary) test to verify this. We will replace this test with a retry
    test in the next commit.

--- src/fake/auth.js
+++ src/fake/auth.js
@@ -90,12 +90,16 @@ export const multitenantAuthData = (config, resolve, reject, fakeTokenStore) => 
         },
       };
     } else if (formData.grant_type === 'multitenant_token_exchange') {
-      success = {
-        ...fakeTokenStore.exchangeToken(formData.subject_token),
-        client_data: {
-          client_id: '08ec69f6-d37e-414d-83eb-324e94afddf0',
-        },
-      };
+      const exchangedToken = fakeTokenStore.exchangeToken(formData.subject_token);
+
+      if (exchangedToken) {
+        success = {
+          ...exchangedToken,
+          client_data: {
+            client_id: '08ec69f6-d37e-414d-83eb-324e94afddf0',
+          },
+        };
+      }
     }
   } else if (hostname === 'invalid.example.com') {
     error = {
