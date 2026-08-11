c1a567048a694b10f1eb536716bc5a75f5336d9e
Mikko Koski <mikko@sharetribe.com>
2026-08-11 13:02:15 +0300

    Add missing scope
    Add missing scope to fake function which creates a new token after refresh.
    
    If scope is missing, the retry interceptor would return null pointer

--- src/fake/token_store.js
+++ src/fake/token_store.js
@@ -202,7 +202,7 @@ const createTokenStore = () => {
           refresh_token: generateRefreshToken(username),
           token_type: 'bearer',
           expires_in: 86400,
-          // TODO missing `scope`
+          scope: 'user',
         },
         user: {
           username,
